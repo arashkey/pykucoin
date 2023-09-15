50de9eb900bd6e0ed7f6d96cb1aa2d726f117d9e
arash <you@example.com>
2023-09-15 15:23:13 +0330

    rename

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 #pip install -r requirements.txt
+from get_data import get_daily_btcusd_candles
+
+
+if __name__ == "__main__":
+    # Get daily BTC/USD candles
+    btcusd_candles = get_daily_btcusd_candles()
+    # Print the DataFrame
+    print(btcusd_candles)
+    
+    # # Generate some example price data
+    # np.random.seed(0)
+    # price_data = np.random.rand(50) * 10 + 90  # Generate random prices between 90 and 100
+
+    # # Create a pandas DataFrame
+    # df = pd.DataFrame(price_data, columns=['Close'])
+
+    # Calculate RSI with a 14-day period
+    (rsi,rsi_sma) = calculate_rsi_sum(btcusd_candles['c'], period=14)
+    
+
+    print(rsi,rsi_sma)
